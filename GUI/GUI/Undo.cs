fc4555415c8c0b01fa363d1d11f8344f180b3371
DeKuczma <awid12@gmail.com>
2019-01-10 22:17:59 +0100

    added background worker

--- GUI/GUI/Undo.cs
+++ GUI/GUI/Undo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InterfaceBridge;
+using System.Threading;
 
 namespace GUI
 {
@@ -18,6 +20,19 @@ namespace GUI
         }
 
         public void ProcessImage(object sender, EventArgs e)
+        {
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += DoWork;
+            backgroundWorker.RunWorkerCompleted += update;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1000);
+        }
+
+        private void update(object sender, RunWorkerCompletedEventArgs e)
         {
             imageOperation.UndoImage();
         }
